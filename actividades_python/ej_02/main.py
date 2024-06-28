f94c998fb464fb1b1f14084eaeb1e2734b1839e8
ARGChief <frani.marinaro@gmail.com>
2024-06-28 16:27:00 -0300

    ejercicio 2

---
+++ actividades_python/ej_02/main.py
@@ -0,0 +1,32 @@
+from gpiozero import RGBLED
+from time import sleep 
+
+#declaro los pines 
+ledquique = RGBLED(13,16,26)
+
+#declaro las funciones 
+def titilatodo():
+    ledquique.color = (1,1,1)
+    sleep(0.01)
+    ledquique.color = (0,0,0)
+
+def titilaGB():
+    ledquique.color = (0,1,1)
+    sleep(0.01)
+    ledquique.color = (0,0,0)
+
+def titilaG():
+    ledquique.color = (0,1,0)
+    sleep(0.01)
+    ledquique.color = (0,0,0)
+
+#declaro el bucle 
+while True:
+    titilatodo()
+    sleep(0.25)
+    titilaG()
+    sleep(0.25)
+    titilaGB()
+    sleep(0.25)
+    titilaG()
+    sleep(0.25)
